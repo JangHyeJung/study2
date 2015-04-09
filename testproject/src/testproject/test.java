a27a8f0f0956f528a5fa3518b221360bcaf4fbc0
hyejung <hyejung@nate.com>
2015-04-10 00:49:40 +0900

    수정1

--- testproject/src/testproject/test.java
+++ testproject/src/testproject/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("git testing");
+		System.out.println("git testing ¼öÁ¤ 1");
 	}
 
 }
